36e7be451108def03949f0e08c205b68c7496f65
Dowglas Barros <dowglasrodrigo@gmail.com>
2020-01-12 05:09:11 -0300

    Adding portinari

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PoModule } from '@portinari/portinari-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule
   ],
   providers: [],
   bootstrap: [AppComponent]

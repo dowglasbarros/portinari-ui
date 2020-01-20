c922113d145e3be6b6061711ae482753ee5b43e6
Dowglas Barros <dowglasrodrigo@gmail.com>
2020-01-20 00:57:42 -0300

    Creating LoginComponent and setting Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PoModule } from '@portinari/portinari-ui';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
+    RouterModule,
     PoModule
   ],
   providers: [],
